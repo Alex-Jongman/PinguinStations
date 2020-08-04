bd6fb57b6d2768b147c50292dc91ee68b53debd5
Alex Jongman <alex.jongman@gmail.com>
2020-08-04 14:50:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pinguin Radio Stations
+
+Pinguin Radio is a Dutch Online Radio Operator that offers a couple of stations.
+Please check out [there site](https://pinguinradio.com/).
+
+Unfortunatly they don't offer their music via a PWA. Therefore I can't add controle their music seperatly with the app volumes.
+This is an experiment to do so by hosting this page as a PWA.
---
+++ pinguin.mjs
@@ -0,0 +1,24 @@
+document.cookie = "SameSite=Strict; Secure";
+
+let pinguinStations = [];
+
+pinguinStations.indie = document.querySelector("#audio-pinguinIndie");
+pinguinStations.classic = document.querySelector("#audio-pinguinClassic");
+
+function stopAllStations() {
+    Object.keys(pinguinStations).forEach(station => {
+        pinguinStations[station].pause();
+    });    
+}
+
+document.querySelector('#btn-stop').addEventListener('click', stopAllStations);
+
+document.querySelector("#btn-pinguinIndie").addEventListener('click', event => {
+    stopAllStations();
+    pinguinStations.indie.play();
+});
+
+document.querySelector("#btn-pinguinClassic").addEventListener('click', event => {
+    stopAllStations();
+     pinguinStations.classic.play();
+});
