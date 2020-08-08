5396fd53994f5d666cc369a166d133c13d49181c
alex.jongman <alex.jongman@hu.nl>
2020-08-08 11:42:04 +0200

    moved the sw register to the html file

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,5 @@
 console.log('SW Startup');
 
-navigator.serviceWorker.register('serviceWorker.js').then(
-    function (reg) {
-        console.log(':-)', reg);
-    },
-    function (err) {
-        console.log(':-(', err);
-    }
-);
-
 self.addEventListener('install', event => console.log('SW installed'));
 self.addEventListener('activate', event => console.log('SW activated'));
 self.addEventListener('fetch', event => {
